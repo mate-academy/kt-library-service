6d27c83f69b9fb365b3a86dd07dc8c03fba22894
igork <igorkorolj@gmail.com>
2024-03-27 22:13:30 +0200

    quick fix

--- src/main/kotlin/mate/academy/Book.kt
+++ src/main/kotlin/mate/academy/Book.kt
@@ -1,4 +1,7 @@
 package mate.academy
 
-data class Book(val title: String, val authors: List<Author>,val publicationYear: Int, val genre: String, val isbn: String ) {
-}
+data class Book(val title: String,
+                val authors: List<Author>,
+                val publicationYear: Int,
+                val genre: String,
+                val isbn: String)
