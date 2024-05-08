6433f4066338e280e0f72a639c90850b6dcb9cb3
Andriy Beha <apgroup.ua@gmail.com>
2024-05-08 11:38:57 +0200

    Fixed requested changes

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,38 +1,23 @@
 package mate.academy
 
-data class Author(val name: String)
-
-data class Book(
-    val authors: List<Author>,
-    val genre: String,
-    val isbn: String,
-    val publicationYear: Int,
-    val title: String
-)
-
 class LibraryService {
     private val books: MutableList<Book> = mutableListOf()
 
     fun addBook(book: Book) {
-        // TODO: implement
         books.add(book)
     }
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
         return books.filter { it.title.lowercase().contains(title.lowercase()) }
     }
 
-
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
         return books.filter { book ->
             book.authors.any { it.name.lowercase().contains(authorName.lowercase()) }
         }
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
         return books.filter { it.genre.equals(genre, ignoreCase = true) }
     }
 }
