c3d46ff6c35a90d352c8abd088646c2f430b0c54
Solomia Bobyak <mia.bobyak@gmail.com>
2024-07-03 18:02:43 +0300

    add implementation

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,21 @@
 package mate.academy
 
 class LibraryService {
+    private val bookList = mutableListOf<Book>()
     fun addBook(book: Book) {
-        // TODO: implement
+        bookList.add(book)
     }
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
+        return bookList.filter { it.title.contains(title, ignoreCase = true) }
     }
 
 
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
+        return bookList.filter { book -> book.authors.any() { it.name.contains(authorName, ignoreCase = true)} }
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
+        return bookList.filter { it.genre.equals(genre, ignoreCase = true) }
     }
 }
