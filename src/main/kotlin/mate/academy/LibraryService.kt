2e01ce9e8cb909fbdb457d48f24efeb8bb026873
TonyH277 <gajjduktoxa@gmail.com>
2024-08-05 13:09:41 +0300

    implement LibraryService

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,22 @@
 package mate.academy
 
 class LibraryService {
+    val lib = ArrayList<Book>()
+
     fun addBook(book: Book) {
-        // TODO: implement
+        lib.add(book)
     }
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
+        return lib.filter { it.title.contains(title, ignoreCase = true) }
     }
 
 
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
+        return lib.filter { it.authors.any { it.name.contains(authorName, ignoreCase = true) } }
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
+        return lib.filter { it.genre.equals(genre, ignoreCase = true) }.toList()
     }
 }
