838b79f9216aad3b9f856a39b7ac4784b47066d6
Artem Klishch <artemklishch@gmail.com>
2024-11-26 20:24:53 +0200

    feat: updated the code

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -5,22 +5,17 @@ import mate.academy.model.Book
 class LibraryService {
     private val library: MutableList<Book> = mutableListOf()
 
-    fun addBook(book: Book) {
-        library.add(book)
-    }
+    fun addBook(book: Book) = library.add(book)
 
-    fun searchByTitle(title: String): List<Book> {
-        return if (title.isEmpty())
-            library
-        else library.filter { it.title.contains(title, true) }
-    }
+    fun searchByTitle(title: String): List<Book> = if (title.isEmpty())
+        library
+    else library.filter { it.title.contains(title, true) }
 
 
-    fun searchByAuthor(authorName: String): List<Book> {
-        return library.filter { it.authors.find { author -> author.name.contains(authorName, true) } != null }
-    }
+    fun searchByAuthor(authorName: String): List<Book> = library
+        .filter { it.authors.find { author -> author.name.contains(authorName, true) } != null }
 
-    fun searchByGenre(genre: String): List<Book> {
-        return library.filter { it.genre.equals(genre, true) }
-    }
+
+    fun searchByGenre(genre: String): List<Book> = library.filter { it.genre.equals(genre, true) }
+
 }
