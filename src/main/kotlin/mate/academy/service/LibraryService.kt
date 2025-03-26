60995cb4b3804fcb43574ecdf2c83c15c780eddc
Serhii Shyian <serhii.shn@gmail.com>
2025-03-26 16:48:42 +0200

    added library-service solution

---
+++ src/main/kotlin/mate/academy/service/LibraryService.kt
@@ -0,0 +1,27 @@
+package mate.academy.service
+
+import mate.academy.model.Book
+
+class LibraryService {
+    private val books = mutableListOf<Book>()
+
+    fun addBook(book: Book) {
+        books.add(book)
+    }
+
+    fun searchByTitle(title: String): List<Book> {
+        return books.filter { it.title.contains(title, ignoreCase = true) }
+    }
+
+    fun searchByAuthor(authorName: String): List<Book> {
+        return books.filter { book ->
+            book.authors.any { author ->
+                author.name.contains(authorName, ignoreCase = true)
+            }
+        }
+    }
+
+    fun searchByGenre(genre: String): List<Book> {
+        return books.filter { it.genre.equals(genre, ignoreCase = true) }
+    }
+}
